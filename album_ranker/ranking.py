1281453e43c6d7693f5b74c5b663cfa2ac1601d9
j-grewal <jeevongrewal@hotmail.co.uk>
2026-06-13 22:15:11 +0100

    Refactored codebase to organise classes better

--- album_ranker/ranking.py
+++ album_ranker/ranking.py
@@ -2,11 +2,84 @@
 The ranking logic for the project
 """
 
-class SortingAlgo:
-    def __init__(self):
+import logging
+
+logger = logging.getLogger(__name__)
+
+class RankingAlgorithm:
+
+    def __init__(self, album_list):
+        self.album_list = album_list
+        self.current_ranking = []
+        self.unranked_albums = album_list.copy()
+
+    def get_next_comparison(self):
         pass
 
-class BinaryInsertionSort(SortingAlgo):
-    def __init__(self):
+    def record_choice(self, winner, loser):
         pass
 
+    def is_complete(self):
+        if len(self.unranked_albums) > 0:
+            return False
+        else:
+            return True
+
+    def get_ranking(self):
+        return self.current_ranking
+
+
+class BinaryInsertionSort(RankingAlgorithm):
+    def __init__(self, album_list):
+        super().__init__(album_list)
+        self.low_index = 0
+        self.high_index = len(album_list) - 1
+        self.middle_index = (self.high_index + self.low_index) // 2
+        self.current_album = album_list[0]
+
+    def get_next_comparison(self):
+        return self.album_list[self.middle_index]
+    
+    
+
+class RankingSession:
+
+    def __init__(self, algorithm=BinaryInsertionSort):
+        self.algorithm = algorithm
+        self._skipped_comparisons = []
+        self._removed_albums = []
+        self._comparison_history = []
+
+    def get_next_comparison(self):
+        self.algorithm.get_next_comparison()
+
+    def get_ranking(self):
+        return self.algorithm.current_ranking
+
+    def record_choice(self, winner, loser):
+        self._comparison_history.append((winner, loser))
+        self.algorithm.record_choice(winner, loser)
+        pass
+
+    def skip_comparison(self, album_a, album_b):
+
+        pass
+
+    def has_comparison_been_made(self, album_a, album_b):
+        if (album_a, album_b) in self._comparison_history or (album_b, album_a) in self._comparison_history:
+            return True
+        else:
+            return False
+
+    def is_complete(self):
+        return self.algorithm.is_complete()
+    
+    def undo_choice(self):
+        self._comparison_history.pop()
+        # Would also need to set the state to the previous comparison too (which can be gotten from the pop)
+        # Would also need to reverse any ranking updates that were made as a result of the previous choice (this may be tricky)
+
+
+
+
+# Make sure that editing these lists of albums is handled copmletely by the class so that if an album is added to the current rankings, it can be removed from the unrakned albums in the same funciton call.

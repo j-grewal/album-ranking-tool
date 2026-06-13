e644f4d20302f13b8c4a8feea6b6f168f15a635f
j-grewal <jeevongrewal@hotmail.co.uk>
2026-06-13 20:49:46 +0100

    Added cli inputs

---
+++ album_ranker/cli.py
@@ -0,0 +1,8 @@
+"""
+Module for handling the command line user interface
+"""
+
+def get_csv_filepath():
+    filepath = input("Please enter the filepath of your csv file:\n")
+    return filepath
+

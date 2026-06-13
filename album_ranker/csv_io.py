abab0b0a73b59ef820941eeeb0572f3b85e1c7b5
j-grewal <jeevongrewal@hotmail.co.uk>
2026-06-13 22:15:32 +0100

    Added logging

--- album_ranker/csv_io.py
+++ album_ranker/csv_io.py
@@ -2,8 +2,12 @@
 Module to handle reading and writing csv files
 """
 
+import logging
+
 from csv import DictReader, DictWriter
-from models import Album
+from album_ranker.album import Album
+
+logger = logging.getLogger(__name__)
 
 def csv_to_list_of_dicts(filepath):
     list_of_dicts = []
@@ -19,16 +23,19 @@ def csv_to_list_of_dicts(filepath):
                     continue
                 else:
                     metadata[key] = row[key]
-            
+
             row_dict["metadata"] = metadata
 
             list_of_dicts.append(row_dict)
+    
+    logger.info(f"Read in {len(list_of_dicts)} rows from CSV file")
 
     return list_of_dicts
 
 
 def list_of_dicts_to_albums(listofdicts):
     list_of_albums = []
+    logger.info(f"Converting list of dicts to a list of album objects")
     for d in listofdicts:
         list_of_albums.append(
             Album(

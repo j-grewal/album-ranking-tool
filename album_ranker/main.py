ee9dd339453be0f61cad1c9642ee235f77ba23ce
j-grewal <jeevongrewal@hotmail.co.uk>
2026-06-13 22:15:47 +0100

    Planned out main codeblock

--- album_ranker/main.py
+++ album_ranker/main.py
@@ -2,19 +2,29 @@
 Main coding block
 """
 
-import cli
+import logging
+
 import csv_io
+import ranking
+
+
+logger = logging.getLogger(__name__)
 
 def main():
     filepath = "data/example.csv"
     list_of_albums = csv_io.list_of_dicts_to_albums(csv_io.csv_to_list_of_dicts(filepath))
-    print(list_of_albums)
-    print(len(list_of_albums))
-    for i in range(0, 4):
-        print(list_of_albums[i].title)
-        print(list_of_albums[i].artist)
-        print(list_of_albums[i].metadata)
+    
+    algorithm = ranking.BinaryInsertionSort(list_of_albums)
+    session = ranking.RankingSession(algorithm)
 
+    while not session.is_complete():
+        # get next comparison pairs
+        # get user choice
+        # record choice
+        pass
+
+# session.get_ranking()
+# save ranking to csv
 
 if __name__ == "__main__":
     main()

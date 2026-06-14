ca61f780eea01fc6919d6852873be81cf753c1bf
j-grewal <jeevongrewal@hotmail.co.uk>
2026-06-14 16:11:18 +0100

    Started to build out test files

--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -2,6 +2,15 @@
 Test module for ranking.py
 """
 
+import pytest
+
+import album_ranker.ranking
+import test_fixtures as tf
+
+
+
 # tests to implement:
-# Empty input
+# Empty album list
+# One-entry album list
 # Check insertion works correctly
+
